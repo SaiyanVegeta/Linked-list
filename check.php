3260d50973ced15455f88f552a28076f5d735554
SaiyanVegeta <yadul.raghav.cse15@itbhu.ac.in>
2018-02-04 12:27:55 +0530

    Add files via upload

---
+++ check.php
@@ -0,0 +1,23 @@
+    <?php
+
+$db_name = "heroku_18b860342c606ec";
+$username = "bd74d541148a94";
+$password  ="3af85450";
+$server = "us-cdbr-iron-east-05.cleardb.net";
+$conn = mysqli_connect($server, $username, $password, $db_name);
+
+
+
+
+    $query=mysqli_query($conn,"SELECT * FROM heroku_18b860342c606ec.dummy");
+    if($query)
+    {
+        while($row=mysqli_fetch_array($query))
+        {
+            $flag[]=$row;
+        }
+
+        print(json_encode($flag));
+    }
+    mysqli_close($conn);
+    ?>
